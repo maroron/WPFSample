0e3075c6b02b8011d5172ed203f53e659038d195
maroron <aadd9e@gmail.com>
2020-09-03 23:29:29 +0900

    update tunneling & bubbling when we click left mouse down

--- WPFSample/Sample8/RooutedEventHandledTest.xaml.cs
+++ WPFSample/Sample8/RooutedEventHandledTest.xaml.cs
@@ -24,6 +24,7 @@ namespace WPFSample.Sample8
             InitializeComponent();
         }
 
+        #region Right mouse down
         private void Button_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             Console.WriteLine("Button_PreviewMouseRightButtonDown");
@@ -53,5 +54,45 @@ namespace WPFSample.Sample8
         {
             Console.WriteLine("DockPanel_MouseRightButtonDown");
         }
+        #endregion
+
+        #region Left mouse down
+        private void DockPanel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("DockPanel_PreviewMouseLeftButtonDown");
+        }
+
+        private void StackPanel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("StackPanel_PreviewMouseLeftButtonDown");
+        }
+
+
+        // コントロールの既定の動作は常にイベントのバブルバージョンで実装する必要がある。
+        // アプリケーション開発者がプレビューイベントを使用してロジックにフックしたり、
+        // コントロール既定の動作をキャンセルしたりすることが可能になる。
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Button_PreviewMouseLeftButtonDown");
+            Console.WriteLine("Handled = true");
+            e.Handled = true;
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Button_MouseLeftButtonDown");
+        }
+
+        private void StackPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("StackPanel_MouseLeftButtonDown");
+        }
+
+        private void DockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("DockPanel_MouseLeftButtonDown");
+        }
+        #endregion
     }
 }
