6492d841c5ca2147130c8a06a8021aead969c6ff
maroron <aadd9e@gmail.com>
2020-09-08 22:30:18 +0900

    update datagrid column name & row background color

--- WPFSample/Sample9/DataGridTest.xaml.cs
+++ WPFSample/Sample9/DataGridTest.xaml.cs
@@ -34,5 +34,35 @@ namespace WPFSample.Sample9
                 }));
             this.dataGrid.ItemsSource = data;
         }
+
+        private void dataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            // プロパティ名から自動生成する列をカスタマイズする
+            switch (e.PropertyName)
+            {
+                case "Name":
+                    e.Column.Header = "名前";
+                    e.Column.DisplayIndex = 0;
+                    break;
+
+                case "Age":
+                    e.Column.Header = "年齢";
+                    e.Column.DisplayIndex = 1;
+                    break;
+
+                case "Gender":
+                    // Gender プロパティは表示しない
+                    e.Cancel = true;
+                    break;
+
+                case "AuthMember":
+                    e.Column.Header = "承認済み";
+                    e.Column.DisplayIndex = 2;
+                    break;
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
     }
 }
