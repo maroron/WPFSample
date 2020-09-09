fa34f8b869502d812527874adf466a8598e5142a
maroron <aadd9e@gmail.com>
2020-09-09 22:49:55 +0900

    add ComboBox column for datagrid

---
+++ WPFSample/Sample9/GenderComboBoxItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSample.Sample9
+{
+    class GenderComboBoxItem
+    {
+        public string Label { get; set; }
+
+        public Gender Value { get; set; }
+    }
+}
