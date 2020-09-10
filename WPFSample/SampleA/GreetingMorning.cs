67fe195279e13badb5461e3689760972133b6bfa
maroron <aadd9e@gmail.com>
2020-09-10 22:43:09 +0900

    add OOP test code commit

---
+++ WPFSample/SampleA/GreetingMorning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFSample.SampleA
+{
+    class GreetingMorning : GreetingBase
+    {
+        public override string GetMessage()
+        {
+            return "Good Morning";
+        }
+    }
+}
