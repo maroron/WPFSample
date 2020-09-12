64b9c485de6fc31d9e4473cd54585bdd98eadfe0
maroron <aadd9e@gmail.com>
2020-09-12 20:55:19 +0900

    add RSS reader sample

---
+++ WPFSample/SampleB/HttpReader.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFSample.SampleB
+{
+    /// <summary>
+    /// HttpReader.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HttpReader : Window
+    {
+        public HttpReader()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // RSS ファイルの取得
+
+        }
+    }
+}
