74dede62a18988645b159386de36ece49ef61e69
maroron <aadd9e@gmail.com>
2020-09-18 23:05:52 +0900

    add test code in canvas test. half way code.

---
+++ WPFSample/Sample2/KeyEvent/RoiKeyEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFSample.Sample2.KeyEvent
+{
+    class RoiKeyEvent : KeyEventBase
+    {
+        public RoiKeyEvent(Window w) : base(w)
+        {
+        }
+
+        protected override bool IsValidKey(Key key)
+        {
+            bool isValid = false;
+            switch (key)
+            {
+                case Key.Left:
+                case Key.Up:
+                case Key.Right:
+                case Key.Down:
+                    isValid = true;
+                    break;
+                default:
+                    isValid = false;
+                    break;
+            }
+            return isValid;
+        }
+
+        protected override void Process(Key key)
+        {
+
+
+        }
+    }
+}
