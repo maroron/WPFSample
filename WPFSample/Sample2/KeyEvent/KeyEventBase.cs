a3806fd269786f6a2cd7cc2795910d731260f434
maroron <aadd9e@gmail.com>
2020-09-18 22:23:44 +0900

    add KeyEventBase Class in Canvas Test sample.

---
+++ WPFSample/Sample2/KeyEvent/KeyEventBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFSample.Sample2.KeyEvent
+{
+    abstract class KeyEventBase
+    {
+        protected Window w;
+
+        public KeyEventBase(Window w)
+        {
+            this.w = w;
+        }
+
+        public void PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Key key = e.Key;
+
+            StartRegistEvent();
+            if (IsValidKey(key))
+            {
+                Process(key);
+            }
+            EndRegistEvent();
+        }
+
+        protected abstract void StartRegistEvent();
+
+        protected virtual bool IsValidKey(Key key) { return true; }
+
+        protected abstract void Process(Key key);
+
+        protected abstract void EndRegistEvent();
+    }
+}
