61935a8a5099ea09c65670c7a1bbc734aadf7ea3
maroron <aadd9e@gmail.com>
2020-09-19 15:14:56 +0900

    add data for roi in canvas test.

---
+++ WPFSample/Sample2/Data/Roi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFSample.Sample2.Data
+{
+    class Roi
+    {
+        public Rect Rect { get; set; }
+
+        public Point Center { get; set; }
+
+        public Roi(double x, double y, double width, double height)
+        {
+            Rect = new Rect(x, y, width, height);
+            Center = new Point(x + 0.5 * width, y + 0.5 * height);
+        }
+    }
+}
