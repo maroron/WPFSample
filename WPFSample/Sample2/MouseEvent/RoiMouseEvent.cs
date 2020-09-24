4cb25bf7674817b0c26ca95eef3582ce1171ce4c
maroron <aadd9e@gmail.com>
2020-09-25 00:11:43 +0900

    add create RoiMouseEvent format in canvas test.

---
+++ WPFSample/Sample2/MouseEvent/RoiMouseEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFSample.Sample2.MouseEvent
+{
+    class RoiMouseEvent : MouseEventBase
+    {
+        public RoiMouseEvent(Window w) : base(w)
+        {
+        }
+
+        protected override void ProcessMouseDown(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("RoiMouseDown");
+        }
+
+        protected override void ProcessMouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("RoiMouse");
+        }
+    }
+}
