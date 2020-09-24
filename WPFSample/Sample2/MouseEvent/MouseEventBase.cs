754369c7d7b08cfe35ba72209458c5a3d1aaecc5
maroron <aadd9e@gmail.com>
2020-09-24 23:54:28 +0900

    add MouseEvent in canvas test.

---
+++ WPFSample/Sample2/MouseEvent/MouseEventBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFSample.Sample2.MouseEvent
+{
+    abstract class MouseEventBase
+    {
+        protected Window w;
+
+        public MouseEventBase(Window w)
+        {
+            this.w = w;
+        }
+
+        public void PreviewMouseDown(object sender, MouseEventArgs e)
+        {
+            StartRegistMouseDown();
+            if (IsValidMouseDown(sender, e))
+            {
+                ProcessMouseDown(sender, e);
+            }
+            EndRegistMouseDown();
+        }
+
+        protected virtual void StartRegistMouseDown() { }
+
+        protected virtual bool IsValidMouseDown(object sender, MouseEventArgs e) { return true; }
+
+        protected abstract void ProcessMouseDown(object sender, MouseEventArgs e);
+
+        protected virtual void EndRegistMouseDown() { }
+
+        private void PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            StartRegistMouseMove();
+            if (IsValidMouseMove(sender, e))
+            {
+                ProcessMouseMove(sender, e);
+            }
+            EndRegistMouseMove();
+        }
+
+        protected virtual void StartRegistMouseMove() { }
+
+        protected virtual bool IsValidMouseMove(object sender, MouseEventArgs e) { return true; }
+
+        protected abstract void ProcessMouseMove(object sender, MouseEventArgs e);
+
+        protected virtual void EndRegistMouseMove() { }
+    }
+}
